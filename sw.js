b2e162405ff19dcfe169e88a9512e4bf416581ac
info497 <info@happy-smile.gifts>
2025-11-19 19:11:33 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+// Simple Service Worker for NightWarm PWA
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
