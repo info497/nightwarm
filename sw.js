9d30e52b3c25173ddef2ea725053936e89ff2e81
info497 <info@happy-smile.gifts>
2025-11-21 23:24:40 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,20 @@
-// Simple Service Worker for NightWarm PWA
-self.addEventListener("install", event => {
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("nightwarm-cache").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./result.html",
+        "./manifest.json",
+        "./icons/icon-192.png",
+        "./icons/icon-512.png"
+      ]);
     })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
   );
 });
